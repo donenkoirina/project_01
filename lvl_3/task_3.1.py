0c513d6f1ddfb37718d959c04f573a54419745a8
donenkoirina <129791093+donenkoirina@users.noreply.github.com>
2023-05-20 23:18:50 +0300

    Done

---
+++ lvl_3/task_3.1.py
@@ -0,0 +1,51 @@
+# Задача 3.1.
+# Создайте класс матрицы (или таблицы).
+# Требования к классу:
+#   - каждая колонка является числом от 1 до n (n любое число, которые вы поставите!)
+#   - в каждой ячейке содержится либо число, либо None
+#   - доступы следующие методы матрицы:
+#       * принимать новые значения, 
+#       * заменять существующие значения, 
+#       * выводить число строк и колонок.
+
+# Пример матрицы 10 на 10 из единиц:
+# [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+#  [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]]
+
+# Примечание! 
+#   - новый класс не запрещено строить на базе существующих типов данных: списков, словарей и тд.
+#   - отображать в таблице/матрице название колонки не обязательно!
+#   - проявите фантазию :)
+
+class Matrix:
+    def __init__(self, n: int):
+        self.n = n
+        self.matrix = [[1] * n] * n
+
+    def set_value(self, row: int, col: int, value):
+        if row < 0 or row >= self.n or col < 0 or col >= self.n:
+            raise ValueError("Invalid row or column index")
+        self.matrix[row][col] = value
+
+    def get_value(self, row: int, col: int):
+        if row < 0 or row >= self.n or col < 0 or col >= self.n:
+            raise ValueError("Invalid row or column index")
+        return self.matrix[row][col]
+
+    def get_num_rows(self) -> int:
+        return self.n
+
+    def get_num_cols(self) -> int:
+        return self.n
+
+# Example usage
+m = Matrix(10)
+print(m.matrix)
